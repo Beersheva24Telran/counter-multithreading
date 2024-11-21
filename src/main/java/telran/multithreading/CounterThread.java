c22592d1204a8c4eb48514246070cd0a35ef13c8
Yuragranovsky <yuragranovsky@gmail.com>
2024-11-21 16:47:08 +0200

    synchronized static method based solution

--- src/main/java/telran/multithreading/CounterThread.java
+++ src/main/java/telran/multithreading/CounterThread.java
@@ -1,11 +1,10 @@
 package telran.multithreading;
 
 import java.time.LocalDateTime;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class CounterThread extends Thread{
     private static  int nIterations;
-    private static AtomicLong counter = new AtomicLong();
+    private static long counter = 0;
     public static int getnIterations() {
         return nIterations;
     }
@@ -13,13 +12,13 @@ public class CounterThread extends Thread{
         CounterThread.nIterations = nIterations;
     }
     public static long getCounter() {
-        return counter.get();
+        return counter;
     }
    
-    private void counterIncrement() {
+    synchronized static private void counterIncrement() {
         LocalDateTime ldt = LocalDateTime.now();
-        System.out.printf("time before incrementing is %s, the current value is %s\n", ldt, counter);
-        counter.incrementAndGet();
+     System.out.printf("time before incrementing is %s, the current value is %s\n", ldt, counter);
+        counter++;
         System.out.printf("time after incrementing is %s, the current value is %s\n", LocalDateTime.now(), counter);
     }
     @Override
